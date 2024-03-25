bf304369d8586b13656d14f464e8195ae973fdad
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-03-25 12:03:18 -0300

    update desafio 02

--- exercicios/desafio02/script.js
+++ exercicios/desafio02/script.js
@@ -1,9 +1,17 @@
 var ano = window.document.getElementById('iano')
 var sex = window.document.getElementById('sex')
+var mas = window.document.getElementById('imas')
+var fem = window.document.getElementById('ifem')
 var msg = window.document.getElementById('msg')
+var teste = window.document.getElementById('teste')
 
 function clicar(){
     var ano2 = Number(ano.value)
     s = (2024 - ano2)
+    if (sex != mas){
+        teste.innerHTML = 'Feminino'
+    }else{
+        teste.innerHTML = 'Masculino'
+    }
     msg.innerHTML = `Você e ${sex} e tem ${s} anos.`
 }
