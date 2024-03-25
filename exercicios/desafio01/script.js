5515f46ec5d2290f65671989fd6f2de68a1e5d90
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-03-24 21:40:34 -0300

    update desafio01

--- exercicios/desafio01/script.js
+++ exercicios/desafio01/script.js
@@ -3,11 +3,13 @@ var teste = window.document.getElementById ('teste')
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
-    var teste = window.document.getElementById ('teste')
     var agora = new Date()
     var hora = agora.getHours()
 
-    msg.innerHTML = `Agora são exatamente ${hora}`
-    teste.innerHTML = `teste`
-
+    msg.innerText = `Agora são exatamente ${hora}`
+    if (hora < 12){
+        
+    }else if(hora < 18){
+        img.style.backgroundImage
+    }
 }
