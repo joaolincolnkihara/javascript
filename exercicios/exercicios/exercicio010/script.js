0c38e6e99e9d70aee8faf587b7f94ac4f19e2bbe
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-03-31 14:04:55 -0300

    +exercicios

---
+++ exercicios/exercicios/exercicio010/script.js
@@ -0,0 +1,16 @@
+var clicou = document.getElementById('clicou')
+function acao1(){
+    clicou.innerHTML = 'voce clicou no primeiro botão'
+}
+
+function acao2(){
+    clicou.innerHTML = 'voce clicou no segundo botão'
+}
+
+function acao3(){
+    clicou.innerHTML = 'voce clicou no terceiro botão'
+}
+
+function acao4(){
+    clicou.innerHTML = 'voce clicou no quarto botão'
+}
