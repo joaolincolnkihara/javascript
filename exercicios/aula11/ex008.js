f3a211a78f4d4f8f99e8a1160a88271ab5251934
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-04-04 11:38:23 -0300

    +

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 81
+console.log(`a velocidade do seu carro e de ${vel}km/h`)
+if(vel > 80){
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO`)
+}else{
+    console.log(`Você esta na velocidade permitida.`)
+}
+console.log(`use sempre cinto de segurança`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pa = 'Brasil'
+console.log(`Vivendo em ${pa}`)
+if (pa != 'Brasil') {
+    console.log(`Você e Estrangeiro!`)
+} else {
+    console.log(`Você e Brasileiro!`)
+}
