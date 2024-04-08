1547f6e055ec3a17ba1abb0462377bf35e14970f
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-04-08 11:13:21 -0300

    +desafio05

---
+++ exercicios/desafio05/script.js
@@ -0,0 +1,12 @@
+function clicar(){
+    var msg = document.getElementById('msg')
+    var n1 = document.getElementById('iinicio')
+    var n2 = document.getElementById('ifim')
+    var passo = document.getElementById('ipasso')
+
+    var c = n1
+
+    while(c = n2){
+        msg.innerHTML = `funciona`
+    }
+}
