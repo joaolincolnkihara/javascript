c3e8bba551308fb56bbb1a2d88c930ad6f286c83
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-04-12 09:02:06 -0300

    + let num = []

---
+++ exercicios/aula15/ambuente.js
@@ -0,0 +1,9 @@
+let num = [6,54,8,98]
+num[4]=50
+num.sort()
+num.push(7)/*adiciona na ultima posição*/
+/*num.length conta todas as casas*/
+/*num.sort cololca em ordem crescente*/
+console.log (`o nosso vetor tem ${num}`)
+console.log(`O primeiro valor do vetor e ${num[0]}`)
+console.log(num.length)
