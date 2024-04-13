fa4a22bc3bd7916eca0dd6b2cd893dc33a96bc0e
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-04-13 11:23:38 -0300

    aula015/ambinte.js + vetornatela.js

---
+++ exercicios/aula15/valornatela.js
@@ -0,0 +1,10 @@
+let valor = [8,4,5,7,9]
+
+
+/*for(let pos=0; pos <valor.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valor[pos]}`)
+}*/
+
+for(let pos in valor){
+    console.log(`A posilção ${pos} tem o valor ${valor[pos]}`)
+}
