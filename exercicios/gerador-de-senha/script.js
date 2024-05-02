b9b52050cc8769d7ca93c3a14050f03227162f95
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-05-01 21:38:32 -0300

    gerador de senha

--- exercicios/gerador-de-senha/script.js
+++ exercicios/gerador-de-senha/script.js
@@ -1,9 +1,30 @@
-function gerar(){
-    let title = document.getElementById('title')
-    let password = document.getElementById('password')
-    let copy = document.getElementById('copy')
-    let passwordcontainer = document.getElementById('container-password-hide')
+let passwordcontainer = document.getElementById('container-password-hide')
 
+let valor = document.getElementById('valor')
+let slider = document.getElementById('islider')
+
+let password = document.getElementById('password')
+
+let charset = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890!@'
+let novaSenha = ''
+
+// para aparecer o valor do slider no texto
+valor.innerHTML = slider.value
+//slider ira colocar o valor no html, sendo 'this' o valor atual
+slider.oninput = function(){
+    //valor recebe valor atual
+    valor.innerHTML = this.value
+}
+
+// function gerar (){
+//     let pass= ""
+//     for( let i=0; n =charset.length; i < slider.value){
+//         pass += charset.charAt(Math.floor(Math.random()*n))
+//     }
+// }
+
+
+function gerar(){
     if (passwordcontainer.style.display == 'none'){
         passwordcontainer.style.display='block'
     }else{
