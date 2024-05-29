f6bbd708a55e99bf9e44b10bb60c079e6fcea7f6
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-05-29 10:19:23 -0300

    javascript na calculadora

--- exercicios/ideia-calculadora/script.js
+++ exercicios/ideia-calculadora/script.js
@@ -1,6 +1,45 @@
 let msg = document.getElementById('msg')
-let resultado = document.getElementById('igual')
 
-function resultado(){
-    msg.innerHTML= 'ai'
+// function resultado(){
+//     msg.innerHTML += 'resultado'
+// }
+
+function nove(){
+    msg.innerHTML += '9'
+}
+
+function oito(){
+    msg.innerHTML += '8'
+}
+
+function sete(){
+    msg.innerHTML += '7'
+}
+
+function seis(){
+    msg.innerHTML += '6'
+}
+
+function cinco(){
+    msg.innerHTML += '5'
+}
+
+function quatro(){
+    msg.innerHTML += '4'
+}
+
+function tres(){
+    msg.innerHTML += '3'
+}
+
+function dois(){
+    msg.innerHTML += '2'
+}
+
+function um(){
+    msg.innerHTML += '1'
+}
+
+function zero(){
+    msg.innerHTML += '0'
 }
