adf4c14ad0d323749cc3f9f7f2446b661924ac23
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-06-03 08:24:33 -0300

    calculadora01

---
+++ exercicios/ideia-calculadora - Copia/script.js
@@ -0,0 +1,57 @@
+let msg = document.getElementById('msg')
+
+ function resultado(){
+    msg.innerHTML = ''
+     msg.innerHTML += `=`
+}
+
+function nove(){
+    msg.innerHTML += '9'
+}
+function oito(){
+    msg.innerHTML += '8'
+}
+function sete(){
+    msg.innerHTML += '7'
+}
+function seis(){
+    msg.innerHTML += '6'
+}
+function cinco(){
+    msg.innerHTML += '5'
+}
+function quatro(){
+    msg.innerHTML += '4'
+}
+function tres(){
+    msg.innerHTML += '3'
+}
+function dois(){
+    msg.innerHTML += '2'
+}
+function um(){
+    msg.innerHTML += '1'
+}
+function zero(){
+    msg.innerHTML += '0'
+}
+function soma(){
+    msg.innerHTML += '+'
+}
+function sub(){
+    msg.innerHTML += '-'
+}
+function mult(){
+    msg.innerHTML += 'x'
+}
+function vir(){
+    msg.innerHTML += ','
+}
+function apagar(){
+    msg.innerHTML = ''
+    // let currentValue = msg.value
+    // if (currentValue > 0){
+    //     msg.value = currentValue.slice(0, -1)
+    // }
+}
+
