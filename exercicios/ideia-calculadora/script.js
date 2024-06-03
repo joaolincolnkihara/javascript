93848e72b491a3176eff1f6964e2b5327a1ee463
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-06-03 12:20:18 -0300

    calculadora 90%

--- exercicios/ideia-calculadora/script.js
+++ exercicios/ideia-calculadora/script.js
@@ -1,57 +1,24 @@
-let msg = document.getElementById('msg')
+let res = document.getElementById('msg')
 
- function resultado(){
-    msg.innerHTML = ''
-     msg.innerHTML += `=`
+
+function insert(num){
+    res.innerHTML+=num
 }
 
-function nove(){
-    msg.innerHTML += '9'
-}
-function oito(){
-    msg.innerHTML += '8'
-}
-function sete(){
-    msg.innerHTML += '7'
-}
-function seis(){
-    msg.innerHTML += '6'
-}
-function cinco(){
-    msg.innerHTML += '5'
-}
-function quatro(){
-    msg.innerHTML += '4'
-}
-function tres(){
-    msg.innerHTML += '3'
-}
-function dois(){
-    msg.innerHTML += '2'
-}
-function um(){
-    msg.innerHTML += '1'
-}
-function zero(){
-    msg.innerHTML += '0'
-}
-function soma(){
-    msg.innerHTML += '+'
-}
-function sub(){
-    msg.innerHTML += '-'
-}
-function mult(){
-    msg.innerHTML += 'x'
-}
-function vir(){
-    msg.innerHTML += ','
-}
 function apagar(){
-    msg.innerHTML = ''
-    // let currentValue = msg.value
-    // if (currentValue > 0){
-    //     msg.value = currentValue.slice(0, -1)
-    // }
+    res.innerHTML=''
 }
 
+function back(){
+    let resultado = document.getElementById('msg').innerHTML
+    document.getElementById('msg').innerHTML=resultado.substring(0, resultado.length -1)
+}
+
+function resultado(){
+    let resultado = document.getElementById('msg').innerHTML
+    if(resultado){
+        res.innerHTML = eval(resultado)
+    }else{
+        res.innerHTML=''
+    }
+}
